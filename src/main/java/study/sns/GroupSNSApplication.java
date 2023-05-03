63b9440d65a0cc5a0958e2c16f4512d6bfadb1d8
Changbum97 <chb2005@naver.com>
2023-05-03 18:04:53 +0900

    FEAT: Project TimeZone 설정

--- src/main/java/study/sns/GroupSNSApplication.java
+++ src/main/java/study/sns/GroupSNSApplication.java
@@ -3,8 +3,16 @@ package study.sns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GroupSNSApplication {
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GroupSNSApplication.class, args);
